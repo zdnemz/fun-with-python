258042600423caf8998d30805b674d33c4ccf137
zidanemz <137695964+zdnemz@users.noreply.github.com>
2024-01-02 15:18:23 +0800

    add rock-paper-scissors

---
+++ src/rock-paper-scissors/main.py
@@ -0,0 +1,27 @@
+import random
+
+print('=' * 30)
+print('   Rock Paper Scissors')
+print('=' * 30 + '\n')
+
+choices = ['rock', 'paper', 'scissors']
+computer_choice = random.choice(choices)
+
+try:
+    user_input = int(input('1. Rock\n2. Paper\n3. Scissors\nEnter your choice (as a number): '))
+
+    if 1 <= user_input <= 3:
+        user_choice = choices[user_input - 1]
+
+        if computer_choice == user_choice:
+            result = 'It\'s a tie!'
+        elif (computer_choice == 'rock' and user_choice == 'paper') or (computer_choice == 'paper' and user_choice == 'scissors') or (computer_choice == 'scissors' and user_choice == 'rock'):
+            result = 'You win!'
+        else:
+            result = 'You lose!'
+
+        print(f'The computer chose {computer_choice}, so: {result}')
+    else:
+        print('Invalid input. Please enter a number between 1 and 3.')
+except ValueError:
+    print('Please enter only numbers.')
